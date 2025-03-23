bb704dac167d8b1ce907d4eec3ef65c47db7dc51
Vasanth <vasanth255001@gmail.com>
2025-03-23 19:34:09 +0530

    Fixed Header Model

--- core/src/main/java/com/aemvilla/core/bean/NavigationBean.java
+++ core/src/main/java/com/aemvilla/core/bean/NavigationBean.java
@@ -9,4 +9,19 @@ public class NavigationBean {
         this.url = url;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- core/src/main/java/com/aemvilla/core/models/HeaderModel.java
+++ core/src/main/java/com/aemvilla/core/models/HeaderModel.java
@@ -1,16 +1,20 @@
 package com.aemvilla.core.models;
 
 import com.aemvilla.core.bean.NavigationBean;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
-@Model(adaptables = Resource.class,
+@Model(adaptables = SlingHttpServletRequest.class,
         adapters = HeaderModel.class,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
         resourceType = HeaderModel.RESOURCE_HEADER)
@@ -20,20 +24,32 @@ public class HeaderModel {
     static final String RESOURCE_HEADER = "aem-villa/components/header-villa";
 
     @ChildResource
-    @ValueMapValue
-    private Resource navmf;
+     Resource navmf;
 
-    @ValueMapValue
-    private List<NavigationBean> getNavigations() {
-        List<NavigationBean> navigation = new ArrayList<>();
+//    @ValueMapValue
+//     List<NavigationBean> getNavigations() {
+//        List<NavigationBean> navigation = new ArrayList<>();
+//
+//        if (navmf != null) {
+//            for (Resource nav : navmf.getChildren()) {
+//                String navText = nav.getValueMap().get("navText", String.class);
+//                String navUrl = nav.getValueMap().get("navUrl", String.class);
+//                navigation.add(new NavigationBean(navText, navUrl));
+//            }
+//        }
+//        return navigation;
+//    }
 
-        if (navmf != null) {
-            for (Resource nav : navmf.getChildren()) {
-                String navText = nav.getValueMap().get("navText", String.class);
-                String navUrl = nav.getValueMap().get("navUrl", String.class);
-                navigation.add(new NavigationBean(navText, navUrl));
-            }
+    public List<NavigationBean> navigation = new ArrayList<>();
+
+    @PostConstruct
+    protected void init(){
+        Iterator<Resource> resourceIterator = navmf.listChildren();
+        while (resourceIterator.hasNext()){
+            Resource resource = resourceIterator.next();
+            navigation.add(new NavigationBean(resource.getValueMap().get("navText", String.class),resource.getValueMap().get("navUrl", String.class))) ;
+
         }
-        return navigation;
+
     }
 }
